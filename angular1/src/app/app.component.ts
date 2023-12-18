6b21c343d7530f8f4df8ec14b03cb55dd06e79d9
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2023-12-18 08:06:17 +0000

    Angular prova 1

---
+++ angular1/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ProvaComponent } from './prova/prova.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,ProvaComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular1';
+}
